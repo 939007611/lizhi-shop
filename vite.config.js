ff0e2b2b330018ed0f2c786eed7d4cf9a6e071dc
939007611 <939007611@qq.com>
2026-04-14 21:59:29 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 👇 仅加这一行！Netlify用根路径，彻底解决资源加载失败
+  base: '/',
+  server: {
+    port: 5174
+  }
 })
